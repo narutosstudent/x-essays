7ca17673d052922f50072df3840aa6a359639c83
Tiger Abrodi <tigerabrodi@gmail.com>
2023-07-29 10:10:50 +0200

    main height

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,8 +1,37 @@
-import { component$ } from '@builder.io/qwik'
+import { component$, useSignal, useTask$ } from '@builder.io/qwik'
+import { isServer } from '@builder.io/qwik/build'
 import type { DocumentHead } from '@builder.io/qwik-city'
 
 export default component$(() => {
-  return <div style={{ color: 'white' }}>hello world</div>
+  const mainHeight = useSignal(0)
+
+  useTask$(() => {
+    if (isServer) {
+      return
+    }
+
+    const windowHeight = window.innerHeight
+    const headerHeight = document.querySelector('header')!.offsetHeight
+    const footerHeight = (document.querySelector('footer') as HTMLElement)
+      .offsetHeight
+
+    const newMainHeight = windowHeight - headerHeight - footerHeight
+    mainHeight.value = newMainHeight
+  })
+
+  return (
+    <main
+      style={{
+        color: 'white',
+        height:
+          mainHeight.value === 0
+            ? 'calc(100vh - 60px - 60px)'
+            : `${mainHeight}px`,
+      }}
+    >
+      hello world
+    </main>
+  )
 })
 
 export const head: DocumentHead = {
