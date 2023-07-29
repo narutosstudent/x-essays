87eeb1f116e4019eb55d6d9673d5be3aebec6a25
Tiger Abrodi <tigerabrodi@gmail.com>
2023-07-29 10:10:41 +0200

    header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,10 +1,13 @@
-import { component$ } from '@builder.io/qwik'
-import styles from './header.module.css'
+import { component$, useStylesScoped$ } from '@builder.io/qwik'
 import { Logo } from '~/icons/x-logo'
 
+import styles from './header.css?inline'
+
 export const Header = component$(() => {
+  useStylesScoped$(styles)
+
   return (
-    <header class={styles.header}>
+    <header>
       <Logo />
       <h1>Essays</h1>
       <span />
